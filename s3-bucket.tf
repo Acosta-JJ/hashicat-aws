1711dc4b17f71d129b008e8870282fdb96a2052c
Jose Acosta <jose.acosta@xaldigital.com>
2023-01-31 09:57:45 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,9 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-
+  bucket = "chipCourseDoneByJoseAcosta"
   version       = "2.8.0"
   acl           = "private"
-  bucket_prefix = "JoseAcosta-chip-course"
+  
   versioning = {
     enabled = true
   }
