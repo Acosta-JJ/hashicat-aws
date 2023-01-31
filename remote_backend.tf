ab1275427476c46d76ad408b387ff263ab2e28e3
Jose Acosta <jose.acosta@xaldigital.com>
2023-01-31 09:45:02 +0000

    Added s3 bucket module

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "chip-course"
     workspaces {
       name = "hashicat-aws"
